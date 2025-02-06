34383fed2f8ea2377a875d89c6b018bf9229e3f0
louhab abderazzak <louhab.abderazzak.pro@gmail.com>
2025-02-06 12:41:17 +0100

    the scroll is finished

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -8,6 +8,7 @@ import Hero from "./components/hero/Hero";
 import IconsSections from "./components/hero/IconsSections";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
+import ScrollToTop from "./components/scroll/ScrollToTop";
 function App() {
   const [theme, colorMode] = useMode();
   return (
@@ -23,6 +24,7 @@ function App() {
             <Main/>
           </Box> 
           <Footer />
+          <ScrollToTop/>
       </ThemeProvider>
     </ColorModeContext.Provider>
   )
